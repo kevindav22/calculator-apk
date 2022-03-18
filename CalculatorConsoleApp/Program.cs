67afe36eddf94063a2a6292916e0ed6271c3bfb9
Fernanda.Putra <kevindapit22@gmail.com>
2022-03-19 00:01:19 +0700

    Add project files.

---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    
+    
+   
+namespace CalculatorConsoleApp
+{
+    class program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            // int a = 10;
+            //int b = 6;
+
+            Console.Write("Inputkan Nilai A = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Inputkan Nilai B = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b,  Perkalian (a, b));
+            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+        static void main(string[] args)
+        {
+
+        }
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+
