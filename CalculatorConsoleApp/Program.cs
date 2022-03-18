1aac571bc6de427c50845865470f093a5a8f81c0
kepin.pery@gmail.com <kepin.pery@gmail.com>
2022-03-19 04:29:43 +0700

    if else if

--- CalculatorConsoleApp/Program.cs
+++ CalculatorConsoleApp/Program.cs
@@ -10,30 +10,6 @@ namespace CalculatorConsoleApp
 {
     class program
     {
-        
-        static void Main(string[] args)
-        {
-            Console.Title = "Aplikasi Calculator";
-
-            // int a = 10;
-            //int b = 6;
-
-            Console.Write("Inputkan Nilai A = ");
-            int a = int.Parse(Console.ReadLine());
-
-            Console.Write("Inputkan Nilai B = ");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine();
-
-            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
-            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b,  Perkalian (a, b));
-            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
-
-            Console.WriteLine("\nTekan sembarang key untuk keluar");
-            Console.ReadKey();
-        }
         static void main(string[] args)
         {
 
@@ -54,6 +30,76 @@ namespace CalculatorConsoleApp
         {
             return a / b;
         }
+        static void Main(string[] args)
+        {
+            char pilihan;
+            Console.WriteLine("Pilih Menu Kalkulator");
+            Console.WriteLine("\n1. Penjumlahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+            Console.WriteLine(" \nMasukan Pilihan Menu : ");
+            pilihan = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            if (pilihan == '1')
+            {
+                Console.Write("Inputkan Nilai A = ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Inputkan Nilai B = ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+                
+            }
+            else if(pilihan == '2')
+            {
+                Console.Write("Inputkan Nilai A = ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Inputkan Nilai B = ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            }
+            else if(pilihan == '3')
+            {
+                Console.Write("Inputkan Nilai A = ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Inputkan Nilai B = ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+
+            }
+            else if(pilihan =='4')
+            {
+                Console.Write("Inputkan Nilai A = ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Inputkan Nilai B = ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            }
+            else
+            {
+                Console.WriteLine("Maaf Menu yang anda pilih tidak tersedia");
+            }
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+        
     }
 }
 
